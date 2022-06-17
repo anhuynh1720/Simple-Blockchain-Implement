d744929acd76fb9406ef26fe2fa0a90077276495
An <hnan18@vp.fitus.edu.vn>
2022-06-17 20:56:07 +0700

    Add method to mine pending transaction

--- src/main.js
+++ src/main.js
@@ -1,44 +1,63 @@
+const { timingSafeEqual } = require('crypto');
 const SHA256 = require('crypto-js/sha256');
 
+class Transaction {
+    constructor(fromAddress, toAddress, amount) {
+        this.fromAddress = fromAddress;
+        this.toAddress = toAddress;
+        this.amount = amount;
+    }
+}
+
 class Block {
-    constructor(index, timestamp, data, previousHash = '' ) {
-        this.index = index;
+    constructor(timestamp, transactions, previousHash = '' ) {
         this.timestamp = timestamp;
-        this.data = data;
+        this.transactions = transactions;
         this.previousHash = previousHash;
         this.hash = this.calculateHash();
+        this.nonce = 0;
     }
 
     calculateHash() {
-        return SHA256(this.index + this.previousHash + this.timestamp + JSON.stringify(this.data)).toString();
+        return SHA256(this.previousHash + this.timestamp + JSON.stringify(this.transactions) + this.nonce).toString();
     }
 
     mineBlock(difficulty) {
-        while(this.hash.substring(0, difficulty) != Array(difficulty + 1).join("0")) {
+        while(this.hash.substring(0, difficulty) !== Array(difficulty + 1).join("0")) {
+            this.nonce++;
             this.hash = this.calculateHash();
         }
 
-        console.log("Block mined: " + this.hash)
+        console.log("Block mined: " + this.hash);
     }
 }
 
 class Blockchain {
     constructor() {
         this.chain = [this.createGenesisBlock()];
+        this.difficulty = 2;
+        this.pendingTransaction = [];
+        this.miningReward = 100;
     }
 
     createGenesisBlock() {
-        return new Block(0, "01/01/2022", "Genesis block", "0");
+        return new Block("01/01/2022", "Genesis block", "0");
     }
 
     getLatestBlock() {
         return this.chain[this.chain.length - 1];
     }
 
-    addBlock(newBlock) {
-        newBlock.previousHash = this.getLatestBlock().hash;
-        newBlock.hash = newBlock.calculateHash();
-        this.chain.push(newBlock);
+    minePendingTransaction(miningRewardAddress) {
+        let block = new Block(Date.now(), this.pendingTransaction);
+        block.mineBlock(this.difficulty);
+
+        console.log("Block successfully mined!");
+        this.chain.push(block);
+
+        this.pendingTransaction = [
+            new Transaction(null, miningRewardAddress, this.miningReward)
+        ]
     }
 
     isChainValid() {
@@ -59,14 +78,11 @@ class Blockchain {
 }
 
 let coin = new Blockchain();
+
+console.log("Mining block 1...");
+
 coin.addBlock(new Block(1, "01/01/2022", {amount: 4}));
+
+console.log("Mining block 2...");
+
 coin.addBlock(new Block(2, "11/01/2022", {amount: 10}));
-
-console.log('Is blockchain valid?' + coin.isChainValid());
-
-// coin.chain[1].data = { amount: 100};
-// coin.chain[1].hash = coin.chain[1].calculateHash();
-
-// console.log('Is blockchain valid?' + coin.isChainValid());
-
-console.log(JSON.stringify(coin, null, 4));
